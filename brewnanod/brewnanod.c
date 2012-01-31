752991a6d8606b31b0ed6080d1f9ece1c45926aa
Michael Schuld <mikeschuld@gmail.com>
2012-01-30 23:28:49 -0700

    Added placeholder files for both deamon and ui projects

---
+++ brewnanod/brewnanod.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define PORT 14417
+
+int main(int argc, char* argv[]) {
+	printf("BrewNano!\n");
+	
+	return 0;
+}
---
+++ brewnanoui/index.php
@@ -0,0 +1,18 @@
+<?php $PORT = 14417; ?>
+<html>
+<head>
+	<link rel="stylesheet" href="css/layout.css" />
+	<title>BrewNano!</title>
+</head>
+<body id="container">
+	<div id="header">
+		<h1>BrewNano!</h1>
+	</div>
+	<div id="content">
+	
+	</div>
+	<div id="footer">
+		BrewNano!
+	</div>
+</body>
+</html>
